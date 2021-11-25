a36eb82a9e24fc5dd507dae3f548c4e373e6017a
KokoOCE <iaramakomarea@gmail.com>
2021-11-25 23:26:02 +1300

    welp u can colelct items and the eyeball thing and chat thing show
    ok

--- Coldd_Moon_Peak/Assets/Scripts/Koko/Hide.cs
+++ Coldd_Moon_Peak/Assets/Scripts/Koko/Hide.cs
@@ -12,6 +12,7 @@ public class Hide : MonoBehaviour
     public GameObject item5;
     public GameObject item6;
 
+    private bool isInside;
     public GameObject tint;
     float tint_timer;
     public float maxTime = 5f;
@@ -25,12 +26,17 @@ public class Hide : MonoBehaviour
     private bool item5bool = false;
     private bool item6bool = false;
 
+
+    public GameObject eyeClosed;
+    public GameObject eyeOpen;
+
     // Start is called before the first frame update
     void Start()
     {
         tint.GetComponent<SpriteRenderer>().material.color = new Color(1f, 1f, 1f, 0.5f);//sets the opacity of the tint sprite to 50%
                                                                                          //puts all objects with the tag "TintObjecT" into the gameObjects array
-
+        eyeOpen.SetActive(false);                                                 //puts all objects with the tag "TintObjecT" into the gameObjects array
+        eyeClosed.SetActive(false);
 
         //makes all the gameobjects tagged with TintObject invisible at the start
         item1.SetActive(false);
@@ -60,6 +66,16 @@ public class Hide : MonoBehaviour
 
                 if (tint_timer > 0) // Shows Objects with tint
                 {
+                    if (isInside)
+                    {
+                        eyeClosed.SetActive(false);
+                        eyeOpen.SetActive(true);
+                    }
+                    if (!isInside)
+                    {
+                        eyeClosed.SetActive(true);
+                        eyeOpen.SetActive(false);
+                    }
                     if (!item1bool)
                     {
                         item1.SetActive(true);
@@ -107,6 +123,7 @@ public class Hide : MonoBehaviour
                 }
             } 
         }
+
         else //user is NOT holding space
         {
             spacekeytime = 0;
@@ -117,46 +134,66 @@ public class Hide : MonoBehaviour
             item5.SetActive(false);
             item6.SetActive(false);
             tint.SetActive(false);
+
+            eyeClosed.SetActive(false);
+            eyeOpen.SetActive(false);
         }
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if(other.gameObject.tag == "collisionTint")
+        {
+            isInside = true;
+        }
         if (other.gameObject.tag == "item1")
         {
             other.gameObject.SetActive(false);
             Debug.Log("You got item 1");
+            isInside = false;
             item1bool = true;
         }
         if (other.gameObject.tag == "item2")
         {
             other.gameObject.SetActive(false);
             Debug.Log("You got item 2");
+            isInside = false;
             item2bool = true;
         }
         if (other.gameObject.tag == "item3")
         {
             other.gameObject.SetActive(false);
             Debug.Log("You got item 3");
+            isInside = false;
             item3bool = true;
         }
         if (other.gameObject.tag == "item4")
         {
             other.gameObject.SetActive(false);
             Debug.Log("You got item 4");
+            isInside = false;
             item4bool = true;
         }
         if (other.gameObject.tag == "item5")
         {
             other.gameObject.SetActive(false);
             Debug.Log("You got item 5");
+            isInside = false;
             item5bool = true;
         }
         if (other.gameObject.tag == "item6")
         {
             other.gameObject.SetActive(false);
             Debug.Log("You got item 6");
+            isInside = false;
             item6bool = true;
         }
 
+    }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "collisionTint")
+        {
+            isInside = false;
+        }
     }
 }

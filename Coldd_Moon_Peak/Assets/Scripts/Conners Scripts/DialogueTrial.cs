6e1445121bee6de943c61e00c6953d1c5dfed8ea
ToriM8 <Victoria.Mckenzie1@mds.ac.nz>
2021-11-27 16:31:22 +1300

    Beginning to change text files

--- Coldd_Moon_Peak/Assets/Scripts/Conners Scripts/DialogueTrial.cs
+++ Coldd_Moon_Peak/Assets/Scripts/Conners Scripts/DialogueTrial.cs
@@ -8,20 +8,53 @@ using System.IO;
 
 public class DialogueTrial : MonoBehaviour
 {
-    public TextAsset dictionaryTextFile;
+    public TextAsset TextFileOne; //Starting script - introduction , gives clue
+    public TextAsset TextFileTwo; //Correct item - this is what I remember
+    public TextAsset TextFileTwoV2; //Incorrect item This isn't right 
+    public TextAsset TextFileThree;
+    public TextAsset TextFileThreeV2;
+    public TextAsset TextFileFour;
+    public TextAsset TextFileFourV2;
+    public TextAsset TextFileFive;
+    public TextAsset TextFileSix;
+
     public Text Words;
     private string theWholeFileAsOneLongString;
+    private string FileTwoLongString;
     private string[] sentence;
+    private string[] sentenceFileTwo;
+    private string[] sentenceFileTwoV2;
+    private string[] sentenceFileThree;
+    private string[] sentenceFileThreeV2;
+    private string[] sentenceFileFour;
+    private string[] sentenceFileFourV2;
+    private string[] sentenceFileFive;
+    private string[] sentenceFileSix;
+
+
     private int index;
     private int side;
     public bool Type;
     private int currentIndex;
     public float typyingSpeed = 0.02f;
+    
+    //eachLineLists
     private List<string> eachLine;
-    void Start()
+    private List<string> eachLineFileTwo;
+    private List<string> eachLineFileTwoV2;//
+    private List<string> eachLineFileThree;
+    private List<string> eachLineFileThreeV2;
+    private List<string> eachLineFileFour;//
+    private List<string> eachLineFileFourV2;
+    private List<string> eachLineFileFive;
+    private List<string> eachLineFileSix;//
+
+    public Hide hideObject;
+   
+    void FileOneSetup()
     {
         string[] sentence = new string[5];
-        theWholeFileAsOneLongString = dictionaryTextFile.text;
+        theWholeFileAsOneLongString = TextFileOne.text;
 
         eachLine = new List<string>();
         eachLine.AddRange(theWholeFileAsOneLongString.Split("\n"[0]));
@@ -31,20 +64,75 @@ public class DialogueTrial : MonoBehaviour
         sentence[1] = eachLine[1];
         sentence[2] = eachLine[2];
         sentence[3] = eachLine[3];
+    } 
+  
+    void FileTwoSetup()
+    {
+        string[] sentenceFileTwo = new string[5];
+        FileTwoLongString = TextFileTwo.text;
 
-        Debug.Log(eachLine[4]);
-        Debug.Log(eachLine[10]);
-
-        Debug.Log(eachLine[0]);
+        eachLineFileTwo = new List<string>();
+        eachLineFileTwo.AddRange(FileTwoLongString.Split("\n"[0]));
 
 
+        sentenceFileTwo[0] = eachLineFileTwo[0];
+        sentenceFileTwo[1] = eachLineFileTwo[1];
+        sentenceFileTwo[2] = eachLineFileTwo[2];
+        sentenceFileTwo[3] = eachLineFileTwo[3];
+    }
+   
+    void FileTwoV2Setup()
+    {
 
+    }
 
+    void FileThreeSetup()
+    {
 
+    }
+
+    void FileThreeV2Setup()
+    {
+
+    }
+
+    void FileFourSetup()
+    {
+
+    }
+
+    void FileFourV2Setup()
+    {
+
+    }
+
+    void FileFiveSetup()
+    {
+
+    }
+
+    void FileSixSetup()
+    {
+
+    }
+
+    void Start()
+    {
+        FileOneSetup();
+        FileTwoSetup();
     }
 
     void Update()
     {
+        if (hideObject.item1bool == false && hideObject.item2bool == false)
+         Print();
+        if (hideObject.item1bool)
+         PrintTwo();
+    }
+    
+    void Print() 
+    {
+        side = 0;
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Type = true;
@@ -277,7 +365,270 @@ public class DialogueTrial : MonoBehaviour
 
         }
     }
+
+    void PrintTwo()
+    {
+                side = 0;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Type = true;
+        }
+        if (side == 0)
+        {
+            //Words.text = eachLine[side];
+            foreach (char Letter in eachLineFileTwo[side].ToCharArray())
+            {
+                if (Letter != '|')
+                {
+                    Words.text += Letter;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Type = false;
+            side++;
+
+        }
+        if (side == 1 && Type == true)
+        {
+            Words.text = "";
+            //Words.text = eachLine[side];
+            foreach (char Letter in eachLineFileTwo[side].ToCharArray())
+            {
+                if (Letter != '|')
+                {
+                    Words.text += Letter;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Type = false;
+            side++;
+
+        }
+        if (side == 2 && Type == true)
+        {
+            Words.text = "";
+            //Words.text = eachLine[side];
+            foreach (char Letter in eachLineFileTwo[side].ToCharArray())
+            {
+                if (Letter != '|')
+                {
+                    Words.text += Letter;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Type = false;
+            side++;
+
+        }
+        if (side == 3 && Type == true)
+        {
+            Words.text = "";
+            //Words.text = eachLine[side];
+            foreach (char Letter in eachLineFileTwo[side].ToCharArray())
+            {
+                if (Letter != '|')
+                {
+                    Words.text += Letter;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Type = false;
+            side++;
+
+        }
+        if (side == 4 && Type == true)
+        {
+            Words.text = "";
+            //Words.text = eachLine[side];
+            foreach (char Letter in eachLineFileTwo[side].ToCharArray())
+            {
+                if (Letter != '|')
+                {
+                    Words.text += Letter;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Type = false;
+            side++;
+
+        }
+        if (side == 5 && Type == true)
+        {
+            Words.text = "";
+            //Words.text = eachLine[side];
+            foreach (char Letter in eachLineFileTwo[side].ToCharArray())
+            {
+                if (Letter != '|')
+                {
+                    Words.text += Letter;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Type = false;
+            side++;
+
+        }
+        if (side == 6 && Type == true)
+        {
+            Words.text = "";
+            //Words.text = eachLine[side];
+            foreach (char Letter in eachLineFileTwo[side].ToCharArray())
+
+            {
+                if (Letter != '|')
+                {
+                    Words.text += Letter;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Type = false;
+            side++;
+
+        }
+        if (side == 7 && Type == true)
+        {
+            Words.text = "";
+            //Words.text = eachLine[side];
+            foreach (char Letter in eachLineFileTwo[side].ToCharArray())
+            {
+                if (Letter != '|')
+                {
+                    Words.text += Letter;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Type = false;
+            side++;
+
+        }
+        if (side == 8 && Type == true)
+        {
+            Words.text = "";
+            //Words.text = eachLine[side];
+            foreach (char Letter in eachLineFileTwo[side].ToCharArray())
+            {
+                if (Letter != '|')
+                {
+                    Words.text += Letter;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Type = false;
+            side++;
+
+        }
+        if (side == 9 && Type == true)
+        {
+            Words.text = "";
+            //Words.text = eachLine[side];
+            foreach (char Letter in eachLineFileTwo[side].ToCharArray())
+            {
+                if (Letter != '|')
+                {
+                    Words.text += Letter;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Type = false;
+            side++;
+
+        }
+        if (side == 10 && Type == true)
+        {
+            Words.text = "";
+            //Words.text = eachLine[side];
+            foreach (char Letter in eachLineFileTwo[side].ToCharArray())
+            {
+                if (Letter != '|')
+                {
+                    Words.text += Letter;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Type = false;
+            side++;
+
+        }
+        if (side == 11 && Type == true)
+        {
+            Words.text = "";
+            //Words.text = eachLine[side];
+            foreach (char Letter in eachLineFileTwo[side].ToCharArray())
+            {
+                if (Letter != '|')
+                {
+                    Words.text += Letter;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Type = false;
+            side++;
+
+        }
+    }
+
+    void PrintTwoV2()
+    {
+
+    }
+    void PrintThree()
+    {
+
+    }
+    void PrintThreeV2()
+    {
+
+    }
+    void PrintFour()
+    {
+
+    }
+    void PrintFourV2()
+    {
+
+    }
+    void PrintFive()
+    {
+
+    }
+    void PrintSix()
+    {
+
+    }
 }
-
-
-    
